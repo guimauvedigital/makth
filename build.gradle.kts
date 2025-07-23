8be3da4ade0ffe875a015765eb6af76893c1a743
NathanFallet <contact@nathanfallet.me>
2025-07-23 18:58:44 +0200

    update license

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,14 @@ plugins {
 allprojects {
     group = "dev.makth"
     version = "1.3.2"
+    project.ext.set("url", "https://github.com/guimauvedigital/makth")
+    project.ext.set("license.name", "Apache 2.0")
+    project.ext.set("license.url", "https://www.apache.org/licenses/LICENSE-2.0.txt")
+    project.ext.set("developer.id", "nathanfallet")
+    project.ext.set("developer.name", "Nathan Fallet")
+    project.ext.set("developer.email", "contact@nathanfallet.me")
+    project.ext.set("developer.url", "https://www.nathanfallet.me")
+    project.ext.set("scm.url", "https://github.com/guimauvedigital/makth.git")
 
     repositories {
         mavenCentral()
